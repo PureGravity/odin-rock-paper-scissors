78e923aec9427216d83b630de07170adf2cf78fb
Jay <49499730+PureGravity@users.noreply.github.com>
2024-07-28 21:10:04 -0400

    Refactor: create endGame() and button effects

--- script.js
+++ script.js
@@ -10,23 +10,38 @@ function getComputerChoice() {
     }
 }
 
-// Returns the user's choice of 'rock', 'paper', or 'scissors'.
-// function getHumanChoice() {
-//     let userHand = prompt('Shoot! [Input Rock, Paper, or Scissors]').toLowerCase();
-//     return userHand;
-// }
-
 let humanScore = 0;
 let computerScore = 0;
+
+function endGame(){
+    let finalResult = document.querySelector('.results p')
+    let leftContent = document.querySelector('.rules p')
+    let rightContent = document.querySelector('.scoreTrack p')
+    
+    let buttons = document.querySelectorAll('.buttons li')
+
+    buttons.forEach(function(button){
+        button.remove()
+    });
+    leftContent.textContent = ''
+    rightContent.textContent = ''
+    if (humanScore > computerScore){
+        finalResult.textContent = "Congrats! You've reached 5 wins first!"
+    } else {
+        finalResult.textContent = "The bot reached 5 wins first. Try again?"
+    }
+}
 
 function playRound(humanChoice, computerChoice) {
     let result = document.querySelector('.results p');
     let score = document.querySelector('.scoreTrack');
 
+
     // Update scores based on choices
     if (humanChoice === 'rock') {
         if (computerChoice === 'paper') {
             computerScore++;
+            console.log(computerScore)
             result.textContent = 'You lose.';
         } else if (computerChoice === 'scissors') {
             humanScore++;
@@ -60,16 +75,29 @@ function playRound(humanChoice, computerChoice) {
     let playDescription = `You chose ${humanChoice}<br>The bot chose ${computerChoice}<br><br>Score: ${humanScore} to ${computerScore}`;
     score.innerHTML = playDescription;
 
+    // Add lead description
     let leadDesc = document.createElement('p')
     leadDesc.innerHTML = ''
     if (humanScore == computerScore){
         leadDesc.textContent = 'The game is tied.';
+        leadDesc.style.color = 'yellow'
     } else if (humanScore > computerScore){
         leadDesc.textContent = 'You are in the lead!';
+        leadDesc.style.color = 'green'
     } else {
         leadDesc.textContent = 'The bot is in the lead!';
+        leadDesc.style.color = 'darkRed'
     }
+
     score.appendChild(leadDesc)
+
+    if (humanScore >= 5 || computerScore >= 5) {
+        console.log('Human: ',humanScore)
+        console.log('Bot:', computerScore)
+        endGame()
+        return;
+    }
+
 }
 
 let rock = document.querySelector('.rock')
@@ -85,3 +113,14 @@ paper.addEventListener('click', function(){
 scissors.addEventListener('click', function(){
     playRound('scissors', getComputerChoice())
 })
+
+
+let buttons = document.querySelectorAll('.buttons li')
+buttons.forEach(function(button){
+    button.addEventListener('mouseover',function(){
+        button.style.color = '#ebcfc6';
+    });
+    button.addEventListener('mouseout', function(){
+        button.style.color = '';
+    });
+});
